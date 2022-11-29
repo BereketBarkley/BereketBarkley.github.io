7a1b848df2da1d1ea9aa41fdda34a61c256e4448
Shiv Shamdasani <shiv.shamdasani24@trinityschoolnyc.org>
2022-11-29 12:20:45 -0500

    Pie chart updated

--- data/generateJSON.js
+++ data/generateJSON.js
@@ -1,34 +1,33 @@
-//characters = dataSet
-//character_info = data_info
-//character_name = schoolName
-//characterStats = dataStats
-//control alt b to seperate lines
 const fs = require('fs');
 
 let dataSet = {};
+let otherSet = {};
 
 let peeps_csv = fs.readFileSync('SHSAT_DATA.csv', 'utf8');
 
 let peeps = peeps_csv.split("\n");
 
-peeps.forEach(function(peep) {
+peeps.forEach(function (peep) {
   let data_info = peep.split(',');
 
   let schoolName = data_info[0];
 
-  if(schoolName != "Offer Summary"){
-    let dataStats = {};
-    dataStats['Native American'] = data_info[1];
-    dataStats['Asian'] = data_info[2];
-    dataStats['Black'] = data_info[3];
-    dataStats['LatinX'] = data_info[4];
-    dataStats['White'] = data_info[5];
-    dataStats['Multi-Racial'] = data_info[6];
-    dataStats['Unknown'] = data_info[7];
-    dataStats['total'] = data_info[8];
+  let dataStats = {};
+  dataStats['Native American'] = data_info[1];
+  dataStats['Asian'] = data_info[2];
+  dataStats['Black'] = data_info[3];
+  dataStats['LatinX'] = data_info[4];
+  dataStats['White'] = data_info[5];
+  dataStats['Multi-Racial'] = data_info[6];
+  dataStats['Unknown'] = data_info[7];
+  dataStats['total'] = data_info[8];
 
+  if (schoolName != "Offer Summary" && schoolName != "Total Testers vs Offers" && schoolName != "Offer Rate") {
     dataSet[schoolName] = dataStats;
+  } else if (schoolName != "Offer Summary") {
+    otherSet[schoolName] = dataStats;
   }
 });
 
 fs.writeFileSync('dataSet.json', JSON.stringify(dataSet), 'utf8');
+fs.writeFileSync('otherSet.json', JSON.stringify(otherSet), 'utf8');
--- src/siteGenerator.js
+++ src/siteGenerator.js
@@ -4,8 +4,10 @@ const fs = require('fs');
 const ejs = require('ejs');
 
 let school_info = JSON.parse(fs.readFileSync('../data/dataSet.json', 'utf8'));
+let other_info = JSON.parse(fs.readFileSync('../data/otherSet.json', 'utf8'));
 let index_template = fs.readFileSync('views/index.ejs', 'utf8');
 let school_template = fs.readFileSync('views/school.ejs', 'utf8');
+let summary_template = fs.readFileSync('views/summary.ejs', 'utf8');
 let about_template = fs.readFileSync('views/about.ejs', 'utf8');
 
 /*
@@ -22,12 +24,23 @@ for (school in school_info) {
   fs.writeFileSync('../public/' + school_info[school].link + '.html', school_html, 'utf8');
 }
 
+for (other in other_info) {
+  other_info[other].link = getBetterFileName(other);
+  let summary_html = ejs.render(summary_template, {
+    filename: __dirname + '/views/summary.ejs',
+    stats: other_info[other],
+    name: other
+  });
+  fs.writeFileSync('../public/' + other_info[other].link + '.html', summary_html, 'utf8');
+}
+
 /*
   1) Generate an index page of all characters
 */
 let index_html = ejs.render(index_template, {
   filename: __dirname + '/views/index.ejs',
-  data: school_info
+  school_data: school_info,
+  other_data: other_info
 });
 
 let about_html = ejs.render(about_template, {
