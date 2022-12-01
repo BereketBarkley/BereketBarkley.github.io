798b39ed09edbdab19a78cd102ae561e552b159e
Shiv Shamdasani <shiv.shamdasani24@trinityschoolnyc.org>
2022-12-01 14:03:19 -0500

    footer

--- src/siteGenerator.js
+++ src/siteGenerator.js
@@ -9,6 +9,7 @@ let index_template = fs.readFileSync('views/index.ejs', 'utf8');
 let school_template = fs.readFileSync('views/school.ejs', 'utf8');
 let summary_template = fs.readFileSync('views/summary.ejs', 'utf8');
 let about_template = fs.readFileSync('views/about.ejs', 'utf8');
+//let conclusions_template = fs.readFileSync('views/conclusions.ejs', 'utf8');
 
 /*
   1) Generate a web page for each character
@@ -48,8 +49,15 @@ let about_html = ejs.render(about_template, {
   data: school_info
 });
 
+/*let conclusions_html = ejs.render(conclusions_template,{
+  filename: __dirname + '/views/conclusions.ejs',
+  data: school_info
+});
+*/
 fs.writeFileSync('../public/index.html', index_html, 'utf8');
 fs.writeFileSync('../public/about.html', about_html, 'utf8');
+//fs.writeFileSync('../public/conclusions.html', conclusions_html, 'utf8');
+
 
 function getBetterFileName(schoolName) {
   let betterFileName = schoolName.split(" ").join("_");
